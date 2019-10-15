6fa604c8065b62277a40d55b8c2aacc106e989d0
ryanrich7 <ryan.richards@stonybrook.edu>
2019-10-15 11:43:33 -0400

    Updating number of critical planes

--- include/g4hrsSteppingAction.hh
+++ include/g4hrsSteppingAction.hh
@@ -71,8 +71,8 @@ class g4hrsSteppingAction : public G4UserSteppingAction
 	
 	int numZCrit;
         int numZCritVar;
-        G4double ZCritData[12][5];
-        G4String ZCritNames[12];
+        G4double ZCritData[23][5];
+        G4String ZCritNames[23];
 
 };
 
